59c804f3b9a4e1a5a40dd0c6b3c6506976e4c7dd
Salma Boudra <redbird7098@gmail.com>
2024-01-17 20:36:43 -0500

    I have changed and backed up all 4 files we have made in class and for exercises.

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,5 @@
+"""My first program for COMP110"""
+
+_author_ = "730563209"
+
+print("Hello there world!")
---
+++ lessons/basics.py
@@ -0,0 +1,5 @@
+"""Demonstrate the basics of a python program"""
+
+print(2)
+print("Howdy!"[4])
+print(type(2<3))
